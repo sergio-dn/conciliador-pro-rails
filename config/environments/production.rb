bd034f7b9a771ed92b983ca1205e5c88e7791bfe
Sergio Domínguez <sergiodominguez@WildLama-SDN.local>
2026-02-03 20:48:38 -0300

    Enable STDOUT logging and static files for better debugging in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,16 +4,24 @@ Rails.application.configure do
   config.enable_reloading = false
   config.eager_load = true
   config.consider_all_requests_local = false
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  
+  # Forzar servidor de archivos estáticos para Render
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || true
+  
   config.force_ssl = true
   config.assume_ssl = true
 
   config.log_tags = [:request_id]
   config.log_level = ENV.fetch('RAILS_LOG_LEVEL', 'info')
 
+  # Forzar logs a la consola de Render
+  if ENV["RAILS_LOG_TO_STDOUT"].present? || true
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   config.action_controller.perform_caching = true
-
   config.active_support.report_deprecations = false
-
   config.i18n.fallbacks = true
 end
